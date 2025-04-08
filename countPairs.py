696cc45764cdb7985c64c6de61314408ed372527
eyoba146 <132304110+eyoba146@users.noreply.github.com>
2025-04-08 05:58:37 -0300

    Update countPairs.py

--- countPairs.py
+++ countPairs.py
@@ -1,11 +1,7 @@
 def countPairs(nums,k):
     count = 0
-    nums.sort()
     for i in range(len(nums)-1):
-        if nums[i] == nums[i+1] and i*(i+1)%k == 0:
-            count+=1
+        for j in range(i+1,len(nums)):
+            if nums[i] == nums[j] and (i*j)%k == 0:
+                    count+=1
     return count
-nums = [3,1,2,2,2,1,3]
-k = 2
-
-print(countPairs(nums,k))
